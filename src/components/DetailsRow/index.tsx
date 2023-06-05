91ed952ee43250224ed6dc7032ac4c7b782380c1
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-06-05 18:35:39 +0200

    create raw city details row component

---
+++ src/components/DetailsRow/index.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+import styles from './styles';
+
+interface Props {
+  title: string;
+  value: string;
+}
+
+const DetailsRow: FC<Props> = ({title, value}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title} numberOfLines={1}>
+        {title}
+      </Text>
+
+      <Text style={styles.value} numberOfLines={1}>
+        {value}
+      </Text>
+    </View>
+  );
+};
+
+export default DetailsRow;
---
+++ src/components/DetailsRow/styles.ts
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+
+// TODO: Style components
+export default StyleSheet.create({
+  container: {},
+  title: {},
+  value: {},
+});
