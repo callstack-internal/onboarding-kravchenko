1d44069a8108a61a1e03b6e5dcce7f7a95148510
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-06-06 11:55:46 +0200

    create weather screen

---
+++ src/screens/Weather/index.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+import styles from './styles';
+
+const WeatherScreen: FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Coming soon...</Text>
+    </View>
+  );
+};
+
+export default WeatherScreen;
