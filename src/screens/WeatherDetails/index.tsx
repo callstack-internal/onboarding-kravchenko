1beaf99724b4b9ac2b3bdbcfd2207faa54851605
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-06-06 15:15:46 +0200

    style weather details

--- src/screens/WeatherDetails/index.tsx
+++ src/screens/WeatherDetails/index.tsx
@@ -1,12 +1,48 @@
 import React, {FC} from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, View} from 'react-native';
+import {RouteProp, useRoute} from '@react-navigation/native';
 
+import CityWeather from '@app/components/CityWeather';
+import DetailsRow from '@app/components/DetailsRow';
+
+// TODO: Add path
+import {RootStackParamList, RootStackScreen} from '../../navigation';
 import styles from './styles';
 
+// TODO: Use real data once API is ready
 const WeatherDetailsScreen: FC = () => {
+  const {
+    params: {cityId},
+  } = useRoute<RouteProp<RootStackParamList, RootStackScreen.WeatherDetails>>();
+
   return (
     <View style={styles.container}>
-      <Text>Coming soon...</Text>
+      <CityWeather
+        style={styles.cityWeatherContainer}
+        cityId={cityId}
+        city="Sumy"
+        status="Clear"
+        iconCode="01d"
+        temperature={22.5}
+        pressable={false}
+      />
+
+      <ScrollView
+        style={styles.detailsContainer}
+        contentContainerStyle={styles.detailsContentContainer}
+        bounces={false}>
+        <DetailsRow title="🌡️ Feels like" value="21 °C" />
+
+        <DetailsRow title="💨 Wind speed" value="5 km/h" />
+
+        <DetailsRow title="☁️ Clouds" value="30 %" />
+
+        <DetailsRow title="👓 Visibility" value="1000 m" />
+
+        <DetailsRow title="🗜️ Pressure" value="1001 hPa" />
+
+        <DetailsRow title="💧 Humidity" value="40 %" />
+      </ScrollView>
     </View>
   );
 };
--- src/screens/WeatherDetails/styles.ts
+++ src/screens/WeatherDetails/styles.ts
@@ -1,7 +1,24 @@
 import {StyleSheet} from 'react-native';
 
+import {colors, sizes} from '@app/theme/index';
+
 export default StyleSheet.create({
   container: {
+    paddingHorizontal: sizes.lg,
+    paddingVertical: sizes.xl,
     flex: 1,
+    backgroundColor: colors.background,
+  },
+  detailsContainer: {
+    flex: 1,
+  },
+  detailsContentContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+    rowGap: sizes.sm,
+  },
+  cityWeatherContainer: {
+    marginBottom: sizes.lg,
   },
 });
