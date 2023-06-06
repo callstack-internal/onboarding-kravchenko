3ee4ddaaa08bb1067fdd5c263ce152a87961feb5
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-06-06 17:44:21 +0200

    align navigation header

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const defaultScreenOptions = {
   },
   headerTintColor: colors.foreground,
   headerBackTitleVisible: false,
+  headerTitleAlign: 'center' as 'center',
 };
 
 const App: FC = () => {
