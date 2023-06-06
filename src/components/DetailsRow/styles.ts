d100f4da0061fe261117de77e8d2fbb1840ec11b
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-06-06 15:15:36 +0200

    style details row

--- src/components/DetailsRow/styles.ts
+++ src/components/DetailsRow/styles.ts
@@ -1,8 +1,22 @@
+import {borderRadius, colors, fonts, sizes} from '@app/theme/index';
 import {StyleSheet} from 'react-native';
 
-// TODO: Style components
+const SQUARE = (sizes.screenWidth - sizes.lg * 2) / 2 - sizes.sm;
+
 export default StyleSheet.create({
-  container: {},
-  title: {},
-  value: {},
+  container: {
+    width: SQUARE,
+    height: SQUARE,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: borderRadius.sm,
+    backgroundColor: colors.secondaryBackground,
+  },
+  title: {
+    ...fonts.caption,
+    marginBottom: sizes.sm,
+  },
+  value: {
+    ...fonts.subheading,
+  },
 });
