2335d34dd124875854ca0ae23460fa91b2ca3a89
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-06-06 14:25:57 +0200

    style weather screen

--- src/screens/Weather/index.tsx
+++ src/screens/Weather/index.tsx
@@ -1,13 +1,70 @@
 import React, {FC} from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, ListRenderItem, View} from 'react-native';
+
+import CityWeather, {
+  Props as CityWeatherProps,
+} from '@app/components/CityWeather';
 
 import styles from './styles';
 
+// TODO: Remove once API is ready
+const CITIES: CityWeatherProps[] = [
+  {
+    city: 'Sumy',
+    status: 'Clear',
+    iconCode: '01d',
+    temperature: 22.5,
+  },
+  {
+    city: 'Kyiv',
+    status: 'Few clouds',
+    iconCode: '02d',
+    temperature: 20.3,
+  },
+  {
+    city: 'Warsaw',
+    status: 'Scattered clouds',
+    iconCode: '03d',
+    temperature: 17.3,
+  },
+  {
+    city: 'Sumy',
+    status: 'Clear',
+    iconCode: '01d',
+    temperature: 22.5,
+  },
+  {
+    city: 'Kyiv',
+    status: 'Few clouds',
+    iconCode: '02d',
+    temperature: 20.3,
+  },
+  {
+    city: 'Warsaw',
+    status: 'Scattered clouds',
+    iconCode: '03d',
+    temperature: 17.3,
+  },
+];
+
 const WeatherScreen: FC = () => {
+  const keyExtractor = (_: CityWeatherProps, index: number) => index.toString();
+
+  const renderItem: ListRenderItem<CityWeatherProps> = ({item}) => (
+    <CityWeather {...item} />
+  );
+
+  const renderSeparator = () => <View style={styles.separator} />;
+
   return (
-    <View style={styles.container}>
-      <Text>Coming soon...</Text>
-    </View>
+    <FlatList
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      data={CITIES}
+      ItemSeparatorComponent={renderSeparator}
+      keyExtractor={keyExtractor}
+      renderItem={renderItem}
+    />
   );
 };
 
